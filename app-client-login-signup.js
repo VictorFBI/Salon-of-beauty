102d0c225c20fa5b4b0d57413e99f9523f34b414
Victor Frolov-Bukanov <vdfrolovbukanov@edu.hse.ru>
2023-08-26 01:41:07 +0300

    third commit

--- app-client-login-signup.js
+++ app-client-login-signup.js
@@ -3,8 +3,35 @@ let login = document.querySelector(".login")
 let slider = document.querySelector(".slider")
 let formSection = document.querySelector(".form-section")
 const signup_btn = document.getElementById('signup-clkbtn')
+const login_btn = document.getElementById('login-clkbtn')
+const text = document.getElementById('text')
+const signup_email = document.getElementById('signup-email')
+const signup_password = document.getElementById('signup-password')
+const confirm_password = document.getElementById('confirm-password')
+const login_email = document.getElementById('login-email')
+const login_password = document.getElementById('login-password')
 
+// TODO
+// Need to fix this avoiding alerts and add checking whether user in the database or not
+login_btn.onclick = function () {
+    if (!validateEmail(login_email.value)) {
+        return alert("Неверный email")
+    }
+    if (login_email.value === "" || login_password.value === "") {
+        return alert("Введите все поля")
+    }
+    window.location.href = 'client-session.html'
+}
 signup_btn.onclick = function () {
+    if (!validateEmail(signup_email.value)) {
+        return alert("Неверный email")
+    }
+    if (text.value === "" || signup_email.value === "" || signup_password.value === "" || confirm_password.value === "") {
+        return alert("Введите все поля")
+    }
+    if (signup_password.value !== confirm_password.value) {
+        return alert("Пароли не совпадают")
+    }
     window.location.href = 'client-first-registration.html'
 }
 
@@ -17,3 +44,11 @@ login.addEventListener("click", () => {
     slider.classList.remove("moveslider")
     formSection.classList.remove("form-section-move")
 });
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
--- app-client-session.js
+++ app-client-session.js
@@ -1,11 +1,18 @@
 let slider = document.querySelector('.slider')
 const record = document.getElementById('record')
 const history_log = document.getElementById('history-log')
+let history_list = document.querySelector('.history-list')
+let record_list = document.querySelector('.record-list')
 
 record.onclick = function () {
     slider.classList.remove("moveslider")
+    history_list.classList.remove("movehistorylist")
+    record_list.classList.remove("moverecordlist")
 }
 
 history_log.onclick = function () {
     slider.classList.add("moveslider")
+    history_list.classList.add("movehistorylist")
+    record_list.classList.add("moverecordlist")
 }
+
